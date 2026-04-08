afa4a4a0ffb2d42d7fba6469d539404e78afbd4f
hokhanhlinh2006 <hokhanhlinh2006@gmail.com>
2026-04-08 09:50:47 +0700

    Add files via upload

---
+++ baitap01_session03/src/main/java/re/baitap01_session03/config/AppConfig.java
@@ -0,0 +1,21 @@
+package re.baitap01_session03.config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("re.baitap01_session03")
+public class AppConfig {
+
+    @Bean
+    public ViewResolver viewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/views/");
+        resolver.setSuffix(".jsp");
+        return resolver;
+    }
+}
